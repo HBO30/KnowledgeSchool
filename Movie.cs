4b7618c6b3f4210a5de82fed68d246d0047b494f
unknown <diab.4869@gmail.com>
2022-06-13 22:03:45 +0300

    initial commit

---
+++ Movie.cs
@@ -0,0 +1,11 @@
+namespace KnowledgeSchool
+{
+    public class Movie
+    {
+        
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Image { get; set; } = string.Empty;
+        public string Url { get; set; } = string.Empty;
+    }
+}
